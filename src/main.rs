d594ff15cfb45f14ee43cc7569191118ab85e2ef
Dustin Carlino <dabreegster@gmail.com>
2024-06-04 11:31:31 +0100

    MVP

---
+++ src/main.rs
@@ -0,0 +1,112 @@
+use anyhow::Result;
+use gdal::{vector::LayerAccess, Dataset};
+use geo::{Geometry, GeometryCollection, Polygon};
+use geojson::{de::deserialize_geometry, Feature, FeatureCollection};
+use indicatif::{ProgressBar, ProgressStyle};
+use serde::Deserialize;
+
+mod pavement;
+
+fn main() -> Result<()> {
+    let pavements = if true {
+        read_gj_input("test_input/small.geojson")?
+    } else {
+        read_gpkg_input("test_input/large.gpkg")?
+    };
+
+    let mut input_polygons = Vec::new();
+    let mut skeletons = Vec::new();
+    let mut perps = Vec::new();
+    let mut thickened = Vec::new();
+
+    let progress = ProgressBar::new(pavements.len() as u64).with_style(ProgressStyle::with_template(
+        "[{elapsed_precise}] [{wide_bar:.cyan/blue}] {human_pos}/{human_len} ({per_sec}, {eta})").unwrap());
+
+    for mut pavement in pavements {
+        progress.inc(1);
+        pavement.calculate();
+
+        input_polygons.push(pavement.polygon);
+        skeletons.extend(pavement.skeletons);
+        perps.extend(pavement.perp_lines);
+        for (polygon, width) in pavement.thickened_lines {
+            let mut f = Feature::from(geojson::Geometry::from(&polygon));
+            f.set_property("width", width);
+            thickened.push(f);
+        }
+    }
+
+    dump_gj("input_polygons.geojson", input_polygons)?;
+    dump_gj("skeletons.geojson", skeletons)?;
+    dump_gj("perps.geojson", perps)?;
+
+    std::fs::write("thickened.geojson", serde_json::to_string(&FeatureCollection {
+        features: thickened,
+        bbox: None,
+        foreign_members: Some(serde_json::json!({
+            "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:EPSG::27700" } }
+        })
+        .as_object()
+        .unwrap()
+        .clone()),
+    })?)?;
+    println!("Wrote thickened.geojson");
+
+    Ok(())
+}
+
+fn dump_gj<IG: Into<Geometry>>(filename: &str, geometry: Vec<IG>) -> Result<()> {
+    let mut fc = FeatureCollection::from(&GeometryCollection::from_iter(geometry));
+    fc.foreign_members = Some(
+        serde_json::json!({
+            "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:EPSG::27700" } }
+        })
+        .as_object()
+        .unwrap()
+        .clone(),
+    );
+    std::fs::write(filename, serde_json::to_string(&fc)?)?;
+    println!("Wrote {filename}");
+    Ok(())
+}
+
+fn read_gj_input(filename: &str) -> Result<Vec<pavement::Pavement>> {
+    let input: Vec<Input> = geojson::de::deserialize_feature_collection_str_to_vec(
+        &std::fs::read_to_string(filename)?,
+    )?;
+    Ok(input
+        .into_iter()
+        .map(|x| pavement::Pavement::new(x.geometry))
+        .collect())
+}
+
+#[derive(Deserialize)]
+struct Input {
+    #[serde(deserialize_with = "deserialize_geometry")]
+    geometry: Polygon,
+}
+
+fn read_gpkg_input(filename: &str) -> Result<Vec<pavement::Pavement>> {
+    let mut results = Vec::new();
+    let dataset = Dataset::open(filename)?;
+    // Assume only one layer
+    let mut layer = dataset.layer(0)?;
+    for feature in layer.features() {
+        if feature
+            .field_as_string_by_name("descriptive_group")?
+            .unwrap()
+            != "Roadside"
+        {
+            continue;
+        }
+        if feature.field_as_string_by_name("make")?.unwrap() != "Manmade" {
+            continue;
+        }
+
+        let Geometry::Polygon(polygon) = feature.geometry().unwrap().to_geo()? else {
+            continue;
+        };
+        results.push(pavement::Pavement::new(polygon));
+    }
+    Ok(results)
+}
---
+++ src/pavement.rs
@@ -0,0 +1,113 @@
+use geo::{Coord, Densify, EuclideanLength, Line, LineIntersection, LineString, Polygon};
+
+pub struct Pavement {
+    // input
+    pub polygon: Polygon,
+
+    // should be center line
+    pub skeletons: Vec<LineString>,
+
+    // regularly spaced lines that measure width
+    pub perp_lines: Vec<Line>,
+
+    // thickened center lines, along with their width
+    pub thickened_lines: Vec<(Polygon, f64)>,
+}
+
+impl Pavement {
+    pub fn new(polygon: Polygon) -> Self {
+        Self {
+            polygon,
+            skeletons: Vec::new(),
+            perp_lines: Vec::new(),
+            thickened_lines: Vec::new(),
+        }
+    }
+
+    pub fn calculate(&mut self) {
+        self.skeletonize();
+        self.make_perp_lines();
+    }
+
+    fn skeletonize(&mut self) {
+        let mut lines = geo_buffer::skeleton_of_polygon_to_linestring(&self.polygon, true);
+        // TODO Dunno what's happening with some
+        lines.retain(|ls| ls.euclidean_length() < 100.0);
+        self.skeletons = lines;
+    }
+
+    fn make_perp_lines(&mut self) {
+        let step_size_meters = 5.0;
+        let project_away_meters = 25.0;
+
+        for skeleton in &self.skeletons {
+            let dense_line = skeleton.densify(step_size_meters);
+
+            // Using lines instead of coords so we can get the angle -- but is this hard to reason
+            // about?  angle_at_point instead?
+            for orig_line in dense_line.lines() {
+                // TODO For the last line, use the last point too
+                let pt = orig_line.start;
+                let angle = line_angle_degrees(orig_line);
+
+                let pt1 = project_away(pt, angle - 90.0, project_away_meters);
+                let pt2 = project_away(pt, angle + 90.0, project_away_meters);
+
+                let Some(perp) = clip_line_to_polygon(&self.polygon, Line::new(pt1, pt2)) else {
+                    continue;
+                };
+                let width = perp.euclidean_length();
+                // TODO Oh hey, happens to be a good heuristic to prune out weird stuff?!
+                if width == 0.0 {
+                    continue;
+                }
+
+                self.perp_lines.push(perp);
+
+                self.thickened_lines
+                    .push((thicken(orig_line, width), width));
+            }
+        }
+    }
+}
+
+fn project_away(pt: Coord, angle_degrees: f64, distance: f64) -> Coord {
+    let (sin, cos) = angle_degrees.to_radians().sin_cos();
+    Coord {
+        x: pt.x + distance * cos,
+        y: pt.y + distance * sin,
+    }
+}
+
+fn line_angle_degrees(line: Line) -> f64 {
+    line.dy().atan2(line.dx()).to_degrees()
+}
+
+fn clip_line_to_polygon(polygon: &Polygon, line: Line) -> Option<Line> {
+    let mut hits = Vec::new();
+    for polygon_line in polygon.exterior().lines() {
+        if let Some(LineIntersection::SinglePoint { intersection, .. }) =
+            geo::algorithm::line_intersection::line_intersection(line, polygon_line)
+        {
+            hits.push(intersection);
+        }
+    }
+    if hits.len() == 2 {
+        return Some(Line::new(hits[0], hits[1]));
+    }
+    None
+}
+
+fn thicken(line: Line, width: f64) -> Polygon {
+    let angle = line_angle_degrees(line);
+    Polygon::new(
+        LineString::new(vec![
+            project_away(line.start, angle - 90.0, width / 2.0),
+            project_away(line.start, angle + 90.0, width / 2.0),
+            project_away(line.end, angle + 90.0, width / 2.0),
+            project_away(line.end, angle - 90.0, width / 2.0),
+            project_away(line.start, angle - 90.0, width / 2.0),
+        ]),
+        Vec::new(),
+    )
+}
